68da54f325bbb94c764ed85d2d029f3292e8027d
stormsha <1414749109@qq.com>
2019-10-27 19:47:55 +0800

    修复评论提交后无法跳转到评论位置的bug

--- static/js/jquery.js
+++ static/js/jquery.js
@@ -1348,94 +1348,94 @@ function() {
         a(".comt-loading").hide();
         a(".comt-error").hide();
         $body = (window.opera) ? (document.compatMode == "CSS1Compat" ? a("html") : a("body")) : a("html,body");
-        a("#commentform").submit(function() {
-            a(".comt-loading").show();
-            $submit.attr("disabled", true).fadeTo("slow", 0.5);
-            if (o) {
-                a("#comment").after('<input type="text" name="edit_id" id="edit_id" value="' + o + '" style="display:none;" />')
-            }
-            a.ajax({
-                url: "/comment/add/",
-                data: a(this).serialize(),
-                type: a(this).attr("method"),
-                error: function(w) {
-                    a(".comt-loading").hide();
-                    a(".comt-error").show().html(w.responseText);
-                    setTimeout(function() {
-                        $submit.attr("disabled", false).fadeTo("slow", 1);
-                        a(".comt-error").fadeOut()
-                    },
-                    3000)
-                },
-                success: function(B) {
-                    console.log('success');
-                    a(".comt-loading").hide();
-                    r.push(a("#comment").val());
-                    a("textarea").each(function() {
-                        this.value = ""
-                    });
-
-
-                    //获取seeion信息，判断用户是否登录
-                    var user_id = "<%=session.getAttribute('uid','')%>";
-                    var nick = $('#author').val();
-                    if (user_id != ''){     //如果用户处于登录状态，展示用户ID
-                        $('#nick').html(nick);
-                    }
-                    else {                 //如果没有登录，证明是游民，游民可以换马甲
-                        $('#nick').html(nick + '&nbsp; <a class="switch-author" href="javascript:;" data-type="switch-author" style="font-size:12px;">换个身份</a>');
-                    }
-
-                    var y = addComment,
-                    A = y.I("cancel-comment-reply-link"),
-                    w = y.I("wp-temp-form-div"),
-                    C = y.I(y.respondId),
-                    x = y.I("comment_post_ID").value,
-                    z = y.I("comment_parent").value;
-                    if (!o && $comments.length) {
-                        n = parseInt($comments.text().match(/\d+/));
-                        $comments.text($comments.text().replace(n, n + 1))
-                    }
-
-                    new_htm = '" id="new_comm_' + k + '"></';
-                    new_htm = (z == "0") ? ('\n<ol style="clear:both;" class="commentlist commentnew' + new_htm + "ol>") : ('\n<ul class="children' + new_htm + "ul>");
-                    ok_htm = '\n<span id="success_' + k + b;
-                    ok_htm += "</span><span></span>\n";
-
-                    if (z == "0") {
-                        if (a("#postcomments .commentlist").length) {
-                            a("#postcomments .commentlist").before(new_htm)
-                        } else {
-                            a("#respond").after(new_htm)
-                        }
-                    } else {
-                        a("#respond").after(new_htm)
-                    }
-                    a("#comment-author-info").slideUp();
-                    console.log(a("#new_comm_" + k));
-                    a("#new_comm_" + k).hide().append(B);
-                    a("#new_comm_" + k + " li").append(ok_htm);
-                    a("#new_comm_" + k).fadeIn(4000);
-                    $body.animate({
-                        scrollTop: a("#new_comm_" + k).offset().top - 200
-                    },
-                    500);
-                    a(".comt-avatar .avatar").attr("src", a(".commentnew .avatar:last").attr("src"));
-                    l();
-                    k++;
-                    o = "";
-                    a("*").remove("#edit_id");
-                    A.style.display = "none";
-                    A.onclick = null;
-                    y.I("comment_parent").value = "0";
-                    if (w && C) {
-                        w.parentNode.insertBefore(C, w);
-                        w.parentNode.removeChild(w)
-                    }
-                }
-            });
-            return false
-        });
+        // a("#commentform").submit(function() {
+        //     a(".comt-loading").show();
+        //     $submit.attr("disabled", true).fadeTo("slow", 0.5);
+        //     if (o) {
+        //         a("#comment").after('<input type="text" name="edit_id" id="edit_id" value="' + o + '" style="display:none;" />')
+        //     }
+        //     a.ajax({
+        //         url: "/comment/add/",
+        //         data: a(this).serialize(),
+        //         type: a(this).attr("method"),
+        //         error: function(w) {
+        //             a(".comt-loading").hide();
+        //             a(".comt-error").show().html(w.responseText);
+        //             setTimeout(function() {
+        //                 $submit.attr("disabled", false).fadeTo("slow", 1);
+        //                 a(".comt-error").fadeOut()
+        //             },
+        //             3000)
+        //         },
+        //         success: function(B) {
+        //             console.log('success');
+        //             a(".comt-loading").hide();
+        //             r.push(a("#comment").val());
+        //             a("textarea").each(function() {
+        //                 this.value = ""
+        //             });
+        //
+        //
+        //             //获取seeion信息，判断用户是否登录
+        //             var user_id = "<%=session.getAttribute('uid','')%>";
+        //             var nick = $('#author').val();
+        //             if (user_id != ''){     //如果用户处于登录状态，展示用户ID
+        //                 $('#nick').html(nick);
+        //             }
+        //             else {                 //如果没有登录，证明是游民，游民可以换马甲
+        //                 $('#nick').html(nick + '&nbsp; <a class="switch-author" href="javascript:;" data-type="switch-author" style="font-size:12px;">换个身份</a>');
+        //             }
+        //
+        //             var y = addComment,
+        //             A = y.I("cancel-comment-reply-link"),
+        //             w = y.I("wp-temp-form-div"),
+        //             C = y.I(y.respondId),
+        //             x = y.I("comment_post_ID").value,
+        //             z = y.I("comment_parent").value;
+        //             if (!o && $comments.length) {
+        //                 n = parseInt($comments.text().match(/\d+/));
+        //                 $comments.text($comments.text().replace(n, n + 1))
+        //             }
+        //
+        //             new_htm = '" id="new_comm_' + k + '"></';
+        //             new_htm = (z == "0") ? ('\n<ol style="clear:both;" class="commentlist commentnew' + new_htm + "ol>") : ('\n<ul class="children' + new_htm + "ul>");
+        //             ok_htm = '\n<span id="success_' + k + b;
+        //             ok_htm += "</span><span></span>\n";
+        //
+        //             if (z == "0") {
+        //                 if (a("#postcomments .commentlist").length) {
+        //                     a("#postcomments .commentlist").before(new_htm)
+        //                 } else {
+        //                     a("#respond").after(new_htm)
+        //                 }
+        //             } else {
+        //                 a("#respond").after(new_htm)
+        //             }
+        //             a("#comment-author-info").slideUp();
+        //             console.log(a("#new_comm_" + k));
+        //             a("#new_comm_" + k).hide().append(B);
+        //             a("#new_comm_" + k + " li").append(ok_htm);
+        //             a("#new_comm_" + k).fadeIn(4000);
+        //             $body.animate({
+        //                 scrollTop: a("#new_comm_" + k).offset().top - 200
+        //             },
+        //             500);
+        //             a(".comt-avatar .avatar").attr("src", a(".commentnew .avatar:last").attr("src"));
+        //             l();
+        //             k++;
+        //             o = "";
+        //             a("*").remove("#edit_id");
+        //             A.style.display = "none";
+        //             A.onclick = null;
+        //             y.I("comment_parent").value = "0";
+        //             if (w && C) {
+        //                 w.parentNode.insertBefore(C, w);
+        //                 w.parentNode.removeChild(w)
+        //             }
+        //         }
+        //     });
+        //     return false
+        // });
         addComment = {
             moveForm: function(z, A, E, y, B) {
                 var I = this,
