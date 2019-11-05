0e6c45723123ad1af5e19a8c1c149cf6a323cc68
stormsha <1414749109@qq.com>
2019-11-05 16:31:02 +0800

    注册model到后台管理

--- apps/comment/admin.py
+++ apps/comment/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import Comment, ArticleComment
+from .models import ArticleComment
 
-admin.site.register(Comment)
 admin.site.register(ArticleComment)
 
