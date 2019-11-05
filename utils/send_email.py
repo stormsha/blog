dc5e2ba0f86842fec2876605708234cbee221905
stormsha <1414749109@qq.com>
2019-11-05 18:22:35 +0800

    修改邮件配置

--- utils/send_email.py
+++ utils/send_email.py
@@ -1,9 +1,17 @@
 import markdown
 import os
+import base64
 from random import Random  # 用于生成随机码
 from django.core.mail import send_mail, EmailMessage  # 发送邮件模块
 from django.conf import settings
+from email.mime.image import MIMEImage
 from user.models import VerifyRecord, Ouser  # 邮箱验证model
+
+
+def add_images(src, img_id):
+    with open(src, 'rb') as img:
+        msg_image = base64.b64encode(img.read())
+    return msg_image
 
 
 # 生成随机字符串
@@ -28,9 +36,6 @@ def common_send_email(email, s_type=None, username=None, content=None):
         email_record.v_type = s_type
         email_record.save()
         email_title = "欢迎注册StormSha的个人主页"
-        # base_url = os.getcwd()
-        # with open(base_url + '\common\\files\html\email.html', 'r', encoding="utf-8") as html:
-        #     content = html.read()
         path = os.path.join(settings.COMMON_DIR, 'files/html/email.html')
         with open(path, 'r', encoding="utf-8") as html:
             content = html.read()
@@ -39,9 +44,13 @@ def common_send_email(email, s_type=None, username=None, content=None):
         if username:
             content = content.replace('(username)', username)
         content = content.replace('(url)', url)
+        image = add_images(os.getcwd() + '/static/images/mychat.png', 'img_cid')
+        if image:
+            content = content.replace('(img)', str(image, 'utf-8'))
         # 发送邮件
         msg = EmailMessage(email_title, content, settings.EMAIL_FROM, [email])
         msg.content_subtype = "html"
+        msg.encoding = 'utf-8'
         msg.send()
     if s_type == "4":
         email_record = VerifyRecord()
