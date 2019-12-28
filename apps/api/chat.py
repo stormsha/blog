f3746da5c2f0c4fc131c6ea51bd4c71a68a5027c
stormsha <1414749109@qq.com>
2019-12-28 15:24:23 +0800

    添加微信公众号token验证

--- apps/api/chat.py
+++ apps/api/chat.py
@@ -17,7 +17,7 @@ class TokenView(APIView):
         timestamp = request.GET.get("timestamp")  # 获取时间戳
         nonce = request.GET.get("nonce")  # 获取随机数
         echostr = request.GET.get("echostr")  # 获取随机字符串
-        token = "echostr"  # 自己设置的token
+        token = "stormsha"  # 自己设置的token
         # 使用字典序排序（按照字母或数字的大小顺序进行排序）
         _list = [token, timestamp, nonce]
         logger.debug(_list)
