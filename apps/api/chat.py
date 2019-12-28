88651756a52ead566635a88c8a98cb733a85fd19
stormsha <1414749109@qq.com>
2019-12-28 15:31:29 +0800

    添加微信公众号token验证

--- apps/api/chat.py
+++ apps/api/chat.py
@@ -17,10 +17,10 @@ class TokenView(APIView):
         timestamp = request.GET.get("timestamp")  # 获取时间戳
         nonce = request.GET.get("nonce")  # 获取随机数
         echostr = request.GET.get("echostr")  # 获取随机字符串
-        logger.info(signature)
-        logger.info(timestamp)
-        logger.info(nonce)
-        logger.info(echostr)
+        logger.info("signature:" + signature)
+        logger.info("timestamp:" + timestamp)
+        logger.info("nonce:" + nonce)
+        logger.info("echostr:" + echostr)
         token = "stormsha"  # 自己设置的token
         # 使用字典序排序（按照字母或数字的大小顺序进行排序）
         _list = [token, timestamp, nonce]
@@ -31,7 +31,7 @@ class TokenView(APIView):
         temp = ''.join(_list)
         sect = hashlib.sha1(temp.encode('utf-8'))
         hashcode = sect.hexdigest()
-        logger.debug(hashcode)
+        logger.info("hashcode:" + hashcode)
         # 将加密后的字符串和signatrue对比，如果相同返回echostr,表示验证成功
         if hashcode == signature:
             return Response(echostr)
