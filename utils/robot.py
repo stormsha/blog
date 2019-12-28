6346394d213ab2a53ed63081b129457ec63cbe80
stormsha <1414749109@qq.com>
2019-12-28 16:51:13 +0800

    添加微信公众号token验证

--- utils/robot.py
+++ utils/robot.py
@@ -1,5 +1,7 @@
+import logging
 from werobot import WeRoBot
 from blog.settings import APP_ID, APP_SECRET, APP_TOKEN
+logger = logging.getLogger(__name__)
 robot = WeRoBot(enable_session=False,
                 token=APP_TOKEN,
                 APP_ID=APP_ID,
@@ -8,4 +10,5 @@ robot = WeRoBot(enable_session=False,
 
 @robot.handler
 def hello(message):
+    logger.info(message)
     return 'Hello world'
