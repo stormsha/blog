8e85e29af8d0637f9607f4e46cee5ad2af519fbd
stormsha <1414749109@qq.com>
2020-03-01 19:53:56 +0800

    redis

--- blog/settings.py
+++ blog/settings.py
@@ -106,15 +106,14 @@ DATABASES = {
 # 使用django-redis缓存页面
 REDIS_HOST = settings_json.get('REDIS_HOST', '127.0.0.1')
 REDIS_PORT = settings_json.get('REDIS_PORT', '6379')
-REDIS_PASS = settings_json.get('REDIS_PASS')
-print("redis://:{}@{}:{}/1".format(REDIS_PASS, REDIS_HOST, REDIS_PORT))
+# REDIS_PASS = settings_json.get('REDIS_PASS')
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://{}:{}".format(REDIS_HOST, REDIS_PORT),
+        "LOCATION": "redis://{}:{}/1".format(REDIS_HOST, REDIS_PORT),
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
-            "PASSWORD": REDIS_PASS
+            # "PASSWORD": REDIS_PASS
         }
     }
 }
