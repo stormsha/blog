30238c2f1d9348a3c32ffad57f5a19cd9b9e6fb9
stormsha <1414749109@qq.com>
2021-12-12 14:18:46 +0800

    实现图片水印

--- apps/storm/search_indexes.py
+++ apps/storm/search_indexes.py
@@ -4,7 +4,7 @@ from haystack import indexes
 from .models import Article
 
 
-class ArticleIndex(indexes.SearchIndex, indexes.Indexable):
+class BlogArticleIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
     id = indexes.IntegerField(model_attr='id')
     views = indexes.IntegerField(model_attr='views')
--- apps/storm/urls.py
+++ apps/storm/urls.py
@@ -1,16 +1,16 @@
 # ---------------------------
-__author__='stormsha'
-__date__='2019/3/10 21:03'
+__author__ = 'stormsha'
+__date__ = '2019/3/10 21:03'
 # ---------------------------
 
-from django.conf.urls import url
+from django.conf.urls import url, include
 from .views import (IndexView, DetailView, MessageView, AboutView, DonateView, ExchangeView,
                     ProjectView, QuestionView, MySearchView, LoveView, LinkView)
 
 urlpatterns = [
     # 首页
     url(r'^$', IndexView.as_view(template_name='index.html'), name='index'),  # 主页，自然排序
-    url(r'^link/$', LinkView, name='link'),     # 申请友情链接
+    url(r'^link/$', LinkView, name='link'),  # 申请友情链接
     url(r'^category/message/$', MessageView, name='message'),
     url(r'^category/about/$', AboutView, name='about'),
     url(r'^category/donate/$', DonateView, name='donate'),
@@ -26,8 +26,9 @@ urlpatterns = [
     # 文章详情页面
     url(r'^article/(?P<slug>.*?)/$', DetailView.as_view(), name='article'),
     # 全文搜索
+    # url(r'^search/$', MySearchView.as_view(), name='search'),
+    # url(r'^search/', include('haystack.urls')),
     url(r'^search/$', MySearchView.as_view(), name='search'),
     # 喜欢
     url(r'^love/$', LoveView, name='love')
 ]
-
